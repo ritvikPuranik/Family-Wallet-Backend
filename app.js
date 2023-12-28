f474e73bf1b75868ac3cc5cd148f94fd78f0d9fa
ritvik puranik <ritvikpuranik@gmail.com>
2023-12-29 00:27:55 +0530

    Login endpoint provided- just a dummy api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const multer = require('multer');
 const Jimp = require("jimp");
 const qrCode = require('qrcode-reader');
@@ -6,7 +7,9 @@ const qrCode = require('qrcode-reader');
 const app = express();
 const port = 3000;
 
-// const upload = multer({ dest: 'uploads/' })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 // Set up multer for handling file uploads
 const storage = multer.memoryStorage();
@@ -20,6 +23,7 @@ app.use((req, res, next) => {
 });
 
 async function decodeQRCode(buffer) {
+  try{
     return new Promise((resolve, reject) => {
       Jimp.read(buffer, async (err, image) => {
         if (err) {
@@ -42,25 +46,37 @@ async function decodeQRCode(buffer) {
         qrcode.decode(image.bitmap);
       });
     });
+  }catch(err){
+    console.log("err while decoding>", err);
+    return null;
+  }
 }
 
 // Handle POST requests to the '/upload' endpoint
 app.post('/upload', upload.single('documentFile'), async(req, res) => {
     try{
-    // const buffer = fs.readFileSync(__dirname + '/uploads/QR1.png');
     const buffer = req.file.buffer;
     
     let code = await decodeQRCode(buffer);
+    if(!code){
+      res.send(null).status(401);
+    }
     code = JSON.parse(code);
     console.log("code json>", code);
 
-    res.send(code);
+    res.send(code).status(201);
 
     }catch(err){
         console.log("error while handling file>>", err);
         res.status(500).send("Internal Server Error");
     }
 });
+
+app.post('/login', async(req, res) =>{
+  console.log("reached login api");
+  console.log("req.body>>", req.body);
+  res.status(201).send(true);
+})
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
